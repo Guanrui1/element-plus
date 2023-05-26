affd6844d345603db5f2fac31125350ff991ba13
guanrui Lu <36908524+Guanrui1@users.noreply.github.com>
2023-05-26 10:42:57 +0800

    Update index.ts

--- packages/components/dialog/index.ts
+++ packages/components/dialog/index.ts
@@ -1,9 +1,9 @@
-import { withInstall } from '@element-plus/utils'
-import Dialog from './src/dialog.vue'
+import { withInstall } from '@element-plus/utils' // 引入 withInstall （组件条件 install 属性用，使用时直接 使用 use 即可安装组件
+import Dialog from './src/dialog.vue' // 引入 Dialog 组件
 
-export const ElDialog = withInstall(Dialog)
-export default ElDialog
+export const ElDialog = withInstall(Dialog) // Dialog 组件添加 install 属性，并重命名为 ElDialog
+export default ElDialog // 导出 ElDialog 组件
 
-export * from './src/use-dialog'
-export * from './src/dialog'
-export * from './src/constants'
+export * from './src/use-dialog' // 导出 use-dialog属性?
+export * from './src/dialog' // 导出 dialog.ts 内容?
+export * from './src/constants' // 导出 constants?
